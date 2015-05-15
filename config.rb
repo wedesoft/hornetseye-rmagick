77dee40725f3f63b11a1e72a314d350c32fc4b90
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 22:43:59 +0100

    Separate gem specification

---
+++ config.rb
@@ -0,0 +1,17 @@
+require 'rake'
+
+PKG_NAME = 'hornetseye-rmagick'
+PKG_VERSION = '1.0.5'
+RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
+TC_FILES = FileList[ 'test/tc_*.rb' ]
+TS_FILES = FileList[ 'test/ts_*.rb' ]
+PKG_FILES = [ 'Rakefile', 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+BIN_FILES = [ 'README.md', 'COPYING', '.document' ] +
+            RB_FILES + TS_FILES + TC_FILES
+SUMMARY = %q{rmagick integration for Hornetseye}
+DESCRIPTION = %q{This Ruby extension implements the necessary conversions to save and load images stored in Hornetseye::MultiArray.}
+LICENSE = 'GPL-3+'
+AUTHOR = %q{Jan Wedekind}
+EMAIL = %q{jan@wedesoft.de}
+HOMEPAGE = %q{http://wedesoft.github.com/hornetseye-rmagick/}
